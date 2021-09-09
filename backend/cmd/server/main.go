2425fd5081332acefeb742e48df9bbb39aa72014
Ian Yong <ianyongyc@gmail.com>
2021-09-09 18:12:27 +0800

    Add support for environment variables

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hansel-app/hansel/internal/adapters/primary/handlers"
+	"github.com/hansel-app/hansel/internal/config"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "127.0.0.1:8000")
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	addr := fmt.Sprintf("localhost:%d", cfg.ServerPort)
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
 	handlers.RegisterServices(s)
+	log.Printf("Starting server on port %d...", cfg.ServerPort)
 	err = s.Serve(l)
 	if err != nil {
 		log.Fatalf("failed to serve: %v", err)
