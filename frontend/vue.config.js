89c40b04ae09347211fd0d54f9c231cb968cd986
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-10 13:55:00 +0800

    Change baseUrl for publishing to GH pages

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    // customises settings for manifest.json
-    
-    // we can plonk other vue-cli plugin options here
+  // customises settings for manifest.json
 
-    pwa: {
-      name: 'frontend',
-      themeColor: '#4DBA87',
-      msTileColor: '#000000',
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
-  
-      // configure the workbox plugin
+  // we can plonk other vue-cli plugin options here
+  publicPath: process.env.CI ? "/hansel/" : "/",
+  pwa: {
+    name: "frontend",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
     //   workboxPluginMode: 'InjectManifest',
     //   workboxOptions: {
     //     // swSrc is required in InjectManifest mode.
     //     swSrc: 'dev/sw.js',
     //     // ...other Workbox options...
     //  }
-    }
-}
+  },
+};
