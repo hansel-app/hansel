0132d05624ef8f7825c42fb5bebd57ca39839e4a
Wu Weiming <59130765+ming-00@users.noreply.github.com>
2021-09-12 15:35:19 +0800

    Revert "Add cycling of gems in map as well as re-centre on user's location button"

--- frontend/src/constants/mapConstants.ts
+++ frontend/src/constants/mapConstants.ts
@@ -5,119 +5,15 @@ export const SINGAPORE_CENTER: hansel.LatLng = Object.freeze({
   lng: 103.8198,
 });
 
-// copy-pasted from https://snazzymaps.com/style/124771/google-maps-clean
-const mapStyles: google.maps.MapTypeStyle[] = [
-  {
-    featureType: "administrative",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "landscape",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.attraction",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.business",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.business",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.business",
-    elementType: "labels.icon",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.government",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.school",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.school",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-];
-
-export const DEFAULT_MAP_CONFIG = Object.freeze({
-  center: SINGAPORE_CENTER,
-  fullscreenControl: false,
+export const DEFAULT_MAP_CONFIG: google.maps.MapOptions = Object.freeze({
   mapTypeControlOptions: {
     mapTypeIds: ["ROADMAP"],
   },
-  maxZoom: 20,
-  minZoom: 16,
+  fullscreenControl: false,
   streetViewControl: false,
-  styles: mapStyles,
-  zoom: 18,
   zoomControl: false,
+  center: SINGAPORE_CENTER,
+  zoom: 18,
+  minZoom: 16,
+  maxZoom: 20,
 });
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,5 @@
-import Vant from "vant";
-import "vant/lib/index.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
 
-createApp(App)
-  .use(Vant)
-  .mount("#app");
+createApp(App).mount('#app')
