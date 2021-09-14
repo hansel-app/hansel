7af6b6addeb276f30ccc61743061a9ca9fdc099c
jialin7878 <E0425498@u.nus.edu>
2021-09-15 03:33:11 +0800

    Add temp interface for Gem

---
+++ frontend/src/interfaces/gem.ts
@@ -0,0 +1,11 @@
+// TODO: replace with interface generated from protobuf?
+export interface Gem {
+  color: GemColor;
+}
+
+export interface GemColor {
+  // Hex code
+  selectionColor: String;
+  tintColor: String;
+}
+
