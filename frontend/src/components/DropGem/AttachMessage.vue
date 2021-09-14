0da2edb253a8bbf4e75ffd1cab63f5081c664cb1
jialin7878 <E0425498@u.nus.edu>
2021-09-15 03:46:16 +0800

    Fill out AttachMessage with components

--- frontend/src/components/DropGem/AttachMessage.vue
+++ frontend/src/components/DropGem/AttachMessage.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1>Attach Message</h1>
+  <GemHeader title="Drop a gem" />
+  <h4>Attach a message!</h4>
+  <Field
+    class="message-input"
+    placeholder="Enter your message"
+    type="textarea"
+    rows="5"
+  />
+  <Button v-on:click="dropGem">Drop gem</Button>
 </template>
+
+<script>
+import { defineComponent } from "vue";
+import { Button, Field } from "vant";
+import GemHeader from "../shared/GemHeader.vue";
+
+export default defineComponent({
+  components: {
+    Button,
+    Field,
+    GemHeader,
+  },
+  methods: {
+    dropGem: function() {
+      console.log("Dropping gem");
+      this.$router.replace({ name: "home" });
+    },
+  },
+});
+</script>
+
+<style scoped>
+.message-input {
+  background-color: #e9e9e9;
+}
+</style>
