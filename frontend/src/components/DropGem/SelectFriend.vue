d5fb2d00b685988c62885a4e3ac1adf3064d62bd
jialin7878 <E0425498@u.nus.edu>
2021-09-15 02:22:55 +0800

    Fill out SelectFriend with components

--- frontend/src/components/DropGem/SelectFriend.vue
+++ frontend/src/components/DropGem/SelectFriend.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Select Friend</h1>
+  <h1>Drop a gem</h1>
+  <h3>Choose a receiver</h3>
+  <Search placeholder="Search a friend" />
+  <CellGroup>
+    <FriendCell v-for="user in mockFriends" :key="user.id" :friend="user" />
+  </CellGroup>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { CellGroup, Search } from "vant";
+import { mockFriends } from "./mockData";
+import FriendCell from "../shared/FriendCell.vue";
+
+export default defineComponent({
+  setup() {},
+  components: {
+    CellGroup,
+    FriendCell,
+    Search,
+  },
+  data() {
+    return {
+      mockFriends,
+    };
+  },
+});
+</script>
