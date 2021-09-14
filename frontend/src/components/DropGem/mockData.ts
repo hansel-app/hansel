de6a16cc9007026d91268a93c17ddbc479ba65d1
jialin7878 <E0425498@u.nus.edu>
2021-09-15 02:20:50 +0800

    Add temp interface for User

---
+++ frontend/src/components/DropGem/mockData.ts
@@ -0,0 +1,8 @@
+import { User } from "@/interfaces";
+
+export const mockFriends: User[] = [
+  { id: 1, username: "Groundskeeper" },
+  { id: 2, username: "Wimp" },
+  { id: 3, username: "Goose" },
+  { id: 4, username: "Pub owner" },
+];
---
+++ frontend/src/interfaces/user.ts
@@ -0,0 +1,5 @@
+// TODO: replace with interface generated from protobuf?
+export interface User {
+  id: number;
+  username: string;
+}
