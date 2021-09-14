43ba72aa9d990aab9261c94ea98c12c2add47fbb
jialin7878 <E0425498@u.nus.edu>
2021-09-14 19:30:01 +0800

    Add route to DropGem

---
+++ frontend/src/components/DropGem.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Drop gem</h1>
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,22 @@
 import Vant from "vant";
-import "vant/lib/index.css";
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
+
+import DropGem from "./components/DropGem.vue";
+import GemMap from "./components/GemMap.vue";
 import App from "./App.vue";
+import "vant/lib/index.css";
 import "./registerServiceWorker";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "home", component: GemMap },
+    { path: "/drop", name: "dropGem", component: DropGem },
+  ],
+});
 
 createApp(App)
   .use(Vant)
+  .use(router)
   .mount("#app");
