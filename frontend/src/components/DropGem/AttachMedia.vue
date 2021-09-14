1e77dabd16a42782e0d28d7267bdfe1217d2a950
jialin7878 <E0425498@u.nus.edu>
2021-09-15 03:34:34 +0800

    Fill out AttachMedia with components

--- frontend/src/components/DropGem/AttachMedia.vue
+++ frontend/src/components/DropGem/AttachMedia.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Attach Media</h1>
+  <GemHeader title="Drop a gem" />
+  <GemColorCarousel />
+  <h4>Choose media</h4>
+  <Row>
+    <Button>Camera</Button>
+    <Button>Photos</Button>
+  </Row>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Button, Row } from "vant";
+import GemHeader from "../shared/GemHeader.vue";
+import GemColorCarousel from "./GemColorCarousel.vue";
+
+export default defineComponent({
+  components: {
+    Button,
+    GemHeader,
+    GemColorCarousel,
+    Row,
+  },
+});
+</script>
