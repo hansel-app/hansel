c6a4043b56e5605b1289e28ffb0bc0f8e696d110
jialin7878 <E0425498@u.nus.edu>
2021-09-15 03:34:02 +0800

    Add shared component GemColorCarousel

---
+++ frontend/src/components/DropGem/GemColorCarousel.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- TODO: make this into actual carousel -->
+  <Row>
+    <Button
+      v-for="gem in mockGems"
+      :key="gem.color"
+      :color="gem.color.selectionColor"
+    />
+  </Row>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Button, Row } from "vant";
+import { mockGems } from "./mockData";
+
+export default defineComponent({
+  data() {
+    return {
+      mockGems,
+    };
+  },
+  components: {
+    Button,
+    Row,
+  },
+  inheritAttrs: false,
+});
+</script>
