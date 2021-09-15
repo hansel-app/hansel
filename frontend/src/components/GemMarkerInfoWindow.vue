a52c39f796371d4b986047e95e6bf8f723024976
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-15 21:03:44 +0800

    Use van layout in info window

--- frontend/src/components/GemMarkerInfoWindow.vue
+++ frontend/src/components/GemMarkerInfoWindow.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    <p>{{ displayDistance }}</p>
-    <p>From {{ dropperName }}</p>
-    <p>{{ displayDropDateTime }}</p>
-  </div>
+  <van-row type="flex" align="center">
+    <van-col span="6">
+      <van-image
+        round
+        fit="cover"
+        src="https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY-970-80.jpg.webp"
+        :show-loading="false"
+        :show-error="false"
+      />
+    </van-col>
+    <van-col span="18">
+      <p>{{ displayDistance }}</p>
+      <p>From {{ dropperName }}</p>
+      <p>{{ displayDropDateTime }}</p>
+    </van-col>
+  </van-row>
 </template>
 
 <script lang="ts">
 import { Dayjs } from "dayjs";
 import { defineComponent } from "vue";
-
 import { formatDistance } from "@/utils/geolocation";
 import { formatDateTime } from "@/utils/date";
+import { Row, Col, Image } from "vant";
 
 export default defineComponent({
+  components: { "van-image": Image, "van-row": Row, "van-col": Col },
   props: {
     distance: {
       type: Number,
