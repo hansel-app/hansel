5a75cfa6261aab0f2e5c84fc09832acfb70131d3
ming-00 <e0470728@u.nus.edu>
2021-09-16 23:54:13 +0800

    Fix CSS for found gem page

--- frontend/src/pages/PickupGem/FoundGem.vue
+++ frontend/src/pages/PickupGem/FoundGem.vue
@@ -1,9 +1,10 @@
 <template>
-  <!-- <div  :style="{'background-image':'url('+backgroundUrl+');'}"> -->
   <div class="bg-container">
     <div class="found-gem">
       <h1>You found a gem!</h1>
-      <button>View contents</button>
+    </div>
+    <div id="hi">
+      <input type="button" class="button" value="View contents">
     </div>
   </div>
 </template>
@@ -19,27 +20,54 @@ export default {
 </script>
 <style scoped>
 .bg-container {
+  font-size: 100%;
+  text-align: center;
   height:100vh;
   position: relative;
   background-image: url("../../assets/purple-gem.png"); 
   background-size: cover;
-  background-repeat: no-repeat;
   background-position: 50% 50%;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
 }
+
 .found-gem {
+  box-sizing: border-box;
+  padding: 2em;
+  display: inline-block;
   left: 0;
   position:absolute;
-  text-align:center;
-  top: 3em;
+  height: 80%;
   width: 100%;
 }
+
+.large-button {
+  position: absolute;
+  bottom: 0;
+  margin: 0 auto;
+}
+
+#hi {
+  position: absolute; 
+  bottom: 20%; 
+  left: 50%; 
+  -webkit-transform: translateX(-50%);
+  transform: translateX(-50%);
+}
+
+.button {
+  padding: 1em 2em;
+  background-color: black;
+  border: none;
+  border-radius: 2em;
+  color: white;
+}
+
 h1 {
   font-family: CircularStd-Black;
   font-size: 3em;
-  color: black;
+  color: rgb(87, 40, 80);
 }
 </style>
