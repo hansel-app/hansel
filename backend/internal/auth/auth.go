cbc0e7f44c44998a2bedcb8a8151e86928c54f5a
Ian Yong <ianyongyc@gmail.com>
2021-09-18 02:41:35 +0800

    Remove go-grpc-middleware library

--- backend/internal/auth/auth.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "context"
-
-func AuthenticateRequest(ctx context.Context) (context.Context, error) {
-	// TODO: Implement authentication logic
-	return ctx, nil
-}
--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,19 +1,14 @@
 package server
 
 import (
-	"github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"github.com/jmoiron/sqlx"
 	"google.golang.org/grpc"
 
 	"github.com/hansel-app/hansel/internal/adapters/primary/handlers"
-	"github.com/hansel-app/hansel/internal/auth"
 )
 
 func New(db *sqlx.DB) *grpc.Server {
-	s := grpc.NewServer(
-		grpc.StreamInterceptor(grpc_auth.StreamServerInterceptor(auth.AuthenticateRequest)),
-		grpc.UnaryInterceptor(grpc_auth.UnaryServerInterceptor(auth.AuthenticateRequest)),
-	)
+	s := grpc.NewServer()
 	handlers.RegisterServices(s, db)
 	return s
 }
