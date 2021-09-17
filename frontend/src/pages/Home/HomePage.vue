2694d3b6fa66292d2babb9be3ccacd6305f565e4
jialin7878 <E0425498@u.nus.edu>
2021-09-17 16:09:40 +0800

    Make goToProfile button a circle avatar

--- frontend/src/pages/Home/HomePage.vue
+++ frontend/src/pages/Home/HomePage.vue
@@ -1,9 +1,18 @@
 <template>
   <Suspense>
     <template #default>
-      <div>
-      <button @click="goToProfile">Profile</button>
-      <gem-map :gems="gems" />
+      <div class="container">
+        <gem-map :gems="gems" />
+        <div class="profile-button">
+          <CircleAvatar
+            :avatarUrl="placeholderAvatarUrl"
+            :radius="3"
+            @click="goToProfile"
+            class="profile-button"
+          >
+            Profile</CircleAvatar
+          >
+        </div>
       </div>
     </template>
     <template #fallback>
@@ -17,10 +26,12 @@
 import { defineComponent } from "vue";
 import { PROFILE_ROUTE } from "@/constants";
 import GemMap from "@/components/GemMap.vue";
+import CircleAvatar from "@/components/CircleAvatar.vue";
 import dayjs from "dayjs";
 
 export default defineComponent({
   components: {
+    CircleAvatar,
     GemMap,
   },
 
@@ -72,7 +83,26 @@ export default defineComponent({
   methods: {
     goToProfile() {
       this.$router.push(PROFILE_ROUTE);
-    }
-  }
+    },
+  },
+  data() {
+    return {
+      // TODO: replace with user's own avatar
+      placeholderAvatarUrl:
+        "https://media.istockphoto.com/vectors/happy-young-woman-watching-into-rounded-frame-isolated-on-white-3d-vector-id1296058958?b=1&k=20&m=1296058958&s=170667a&w=0&h=6m2FU2hKv6emHjNtdNSqBJR1uMq64smptqwDAZNo6bg=",
+    };
+  },
 });
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+}
+.container .profile-button {
+  position: absolute;
+  z-index: 10;
+  top: 4px;
+  left: 4px;
+}
+</style>
