41c543852755f424396369b7f045634cee2c47f2
jialin7878 <E0425498@u.nus.edu>
2021-09-18 02:13:22 +0800

    Update AddFriendsPage

--- frontend/src/pages/Friend/AddFriendsPage.vue
+++ frontend/src/pages/Friend/AddFriendsPage.vue
@@ -1,3 +1,22 @@
 <template>
+  <NavBar left-arrow @click-left="goBack" />
   <h2>Add friends</h2>
+  <Search placeholder="Search by username" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { NavBar, Search } from "vant";
+
+export default defineComponent({
+  components: {
+    NavBar,
+    Search,
+  },
+  methods: {
+    goBack() {
+      this.$router.back();
+    },
+  },
+});
+</script>
