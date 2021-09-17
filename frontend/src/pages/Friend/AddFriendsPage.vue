aa1864d973f3afd18814cbcd11f75ef6512c9166
jialin7878 <E0425498@u.nus.edu>
2021-09-18 02:39:05 +0800

    Add filtering usernames for search bar

--- frontend/src/pages/Friend/AddFriendsPage.vue
+++ frontend/src/pages/Friend/AddFriendsPage.vue
@@ -1,17 +1,47 @@
 <template>
   <NavBar left-arrow @click-left="goBack" />
   <h2>Add friends</h2>
-  <Search placeholder="Search by username" />
+  <Search placeholder="Search by username" v-model="searchQuery" />
+  <CellGroup>
+    <FriendCell
+      v-for="user in filteredUsers"
+      :key="user.id"
+      :friend="user"
+      :shouldDisplayUsername="true"
+    />
+  </CellGroup>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { NavBar, Search } from "vant";
+import { CellGroup, NavBar, Search } from "vant";
+import { mockFriends } from "@/interfaces/mockData";
+import FriendCell from "@/components/FriendCell.vue";
 
 export default defineComponent({
   components: {
+    CellGroup,
+    FriendCell,
     NavBar,
     Search,
+  },
+  data() {
+    return {
+      mockFriends,
+      searchQuery: '',
+    };
+  },
+  computed: {
+      filteredUsers() {
+          if (!this.searchQuery) {
+              // Don't display any users when search bar is empty.
+              return [];
+          }
+          return mockFriends.filter(
+              user => user.username.indexOf(this.searchQuery) === 0
+          );
+      }
+
   },
   methods: {
     goBack() {
