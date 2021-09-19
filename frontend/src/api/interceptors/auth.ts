25b45cc3ccb2fa0bf6f85ff5193266bb1b82da61
Ian Yong <ianyongyc@gmail.com>
2021-09-19 19:58:20 +0800

    Set access token on frontend after logging in

--- frontend/src/api/interceptors/auth.ts
+++ frontend/src/api/interceptors/auth.ts
@@ -1,20 +1,30 @@
-import { ClientReadableStream, Request, UnaryResponse } from 'grpc-web';
+import { Request, UnaryResponse } from 'grpc-web';
 import { Message } from 'google-protobuf';
+import { LoginResponse } from "@/protobuf/auth_pb";
+
+const AUTHORIZATION_HEADER_PREFIX = 'Bearer ';
 
 class AuthInterceptor {
   private token: string;
 
-  constructor(token: string) {
-    this.token = token;
+  constructor(token?: string) {
+    this.token = token ?? "";
   }
 
   intercept(
     request: Request<Message, Message>,
-    invoker: (request: Request<Message, Message>) => UnaryResponse<Message, Message> | ClientReadableStream<Message>
-  ): UnaryResponse<Message, Message> | ClientReadableStream<Message> {
-    const metadata = request.getMetadata()
-    metadata.Authorization = 'Bearer ' + this.token
-    return invoker(request)
+    invoker: (request: Request<Message, Message>) => Promise<UnaryResponse<Message, Message>>
+  ): Promise<UnaryResponse<Message, Message>> {
+    const metadata = request.getMetadata();
+    metadata.Authorization = AUTHORIZATION_HEADER_PREFIX + this.token;
+
+    return invoker(request).then((response: UnaryResponse<Message, Message>) => {
+      const responseMessage = response.getResponseMessage();
+      if (responseMessage instanceof LoginResponse) {
+        this.token = responseMessage.getAccessToken();
+      }
+      return response;
+    });
   }
 }
 
--- frontend/src/api/services.ts
+++ frontend/src/api/services.ts
@@ -6,11 +6,12 @@ const SERVER_PORT = process.env.VUE_APP_SERVER_PORT;
 const hostname = `${SERVER_URL}:${SERVER_PORT}`;
 
 // TODO: Inject token.
-const authInterceptor = new AuthInterceptor("");
+const authInterceptor = new AuthInterceptor();
 
+// Note that the equivalent stream authentication interceptor is not defined.
+// If we need to protect stream RPC calls, then it will have to be implemented.
 const options = {
-  unaryInterceptors: [authInterceptor],
-  streamInterceptors: [authInterceptor]
+  unaryInterceptors: [authInterceptor]
 };
 
 const services = Object.freeze({
