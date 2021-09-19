92d2c401c663ab65e09665004089ec0ac087b12f
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-19 17:19:54 +0800

    Refactor components to use new gem interface

--- frontend/src/pages/PickupGem/MessageDisplay.vue
+++ frontend/src/pages/PickupGem/MessageDisplay.vue
@@ -1,43 +1,44 @@
 <template>
   <div class="bg-container" :style="addBackground">
-    <BottomSheet :gemPackage="gemPackage"/>
+    <BottomSheet :gem="gem" />
   </div>
 </template>
-<script  lang="ts">
+<script lang="ts">
 import { defineComponent, PropType } from "vue";
-import { GemInfo } from "@/interfaces";
+import { Gem } from "@/interfaces";
 import BottomSheet from "@/components/BottomSheet.vue";
 
 export default defineComponent({
   props: {
-    gemPackage: {
-      type: Object as PropType<GemInfo>,
+    gem: {
+      type: Object as PropType<Gem>,
+      required: true,
     },
   },
   components: {
-    BottomSheet
+    BottomSheet,
   },
   data() {
     return {
       show: true,
       addBackground: {
         // TODO: get and chuck image here
-        backgroundImage: `url(${require('@/assets/images/gem-found-yellow.png')})`
+        backgroundImage: `url(${require("@/assets/images/gem-found-yellow.png")})`,
       },
     };
   },
   methods: {
     showPopup() {
       this.show = true;
-    }
-  }
+    },
+  },
 });
 </script>
 <style scoped>
 .bg-container {
   font-size: 100%;
   text-align: center;
-  height:100vh;
+  height: 100vh;
   position: relative;
   background-size: cover;
   background-position: 50% 50%;
