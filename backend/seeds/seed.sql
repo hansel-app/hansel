066a7e5500bc44bcd8c95584ae7e4ccade20416f
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-19 20:41:42 +0800

    Change seed gem colours to blue

--- backend/seeds/seed.sql
+++ backend/seeds/seed.sql
@@ -5,9 +5,9 @@ VALUES
 
 INSERT INTO gems (message, latitude, longitude, creator_id, receiver_id, color)
 VALUES
-  ('hello tommy', 1.2966, 103.7764, 1, 2, 0),
-  ('hello tommy', 1.3483, 103.6831, 1, 2, 1),
+  ('hello tommy', 1.2966, 103.7764, 1, 2, 2),
+  ('hello tommy', 1.3483, 103.6831, 1, 2, 2),
   ('hello tommy', 1.3644, 103.9915, 1, 2, 2),
-  ('hello tommy', 1.4382, 103.7891, 1, 2, 3),
-  ('hello tommy', 1.3109, 103.7952, 1, 2, 4),
-  ('hello tommy', 1.3309, 103.8752, 1, 2, 5);
+  ('hello tommy', 1.4382, 103.7891, 1, 2, 2),
+  ('hello tommy', 1.3109, 103.7952, 1, 2, 2),
+  ('hello tommy', 1.3309, 103.8752, 1, 2, 2);
