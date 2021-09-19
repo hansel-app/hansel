c7aa044aff686d3f03ceeacfe6fcc062cb7f0a0b
jialin7878 <E0425498@u.nus.edu>
2021-09-19 15:59:59 +0800

    Fill LoginPage with components

--- frontend/src/pages/LoginRegister/LoginPage.vue
+++ frontend/src/pages/LoginRegister/LoginPage.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Login</h1>
+  <h1>Login</h1>
+  <div class="container">
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="Username"
+        placeholder="Username"
+        :rules="[{ required: true, message: 'Username is required' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="Password"
+        placeholder="Password"
+        :rules="[{ required: true, message: 'Password is required' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="primary" native-type="submit" width="80%">
+          Login
+        </van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
+
+<style scoped lang="less">
+.container .van-field {
+  width: 80%;
+}
+</style>
