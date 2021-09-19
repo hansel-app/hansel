4708c65b10bbccdc3ac6af9e53022783f3d34597
Ian Yong <ianyongyc@gmail.com>
2021-09-19 22:42:26 +0800

    Add login logic to login page

--- frontend/src/pages/LoginRegister/LoginPage.vue
+++ frontend/src/pages/LoginRegister/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Login</h1>
   <div class="container">
-    <van-form @submit="onSubmit">
+    <van-form @submit="handleLogin">
       <van-field
         v-model="username"
         name="Username"
@@ -23,6 +23,34 @@
     </van-form>
   </div>
 </template>
+
+<script>
+import { defineComponent, inject } from "vue";
+import { LOGIN } from "@/providers/AuthProvider"
+import {LOGIN_ROUTE} from "@/constants";
+
+export default defineComponent({
+  setup() {
+    const login = inject(LOGIN, () => Promise.resolve(false));
+    return {
+      login
+    };
+  },
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.login(this.username, this.password)
+        .then(() => this.$router.push(LOGIN_ROUTE))
+        .catch((err) => console.log(err, "Failed to log in"));
+    }
+  }
+});
+</script>
 
 <style scoped lang="less">
 .container .van-field {
