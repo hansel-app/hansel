a9b02cc0e1f0011277056a67e4dfb0bab5de48cd
Ian Yong <ianyongyc@gmail.com>
2021-09-20 17:10:08 +0800

    Hook AuthInterceptor up to the auth store

--- frontend/src/api/interceptors/auth.ts
+++ frontend/src/api/interceptors/auth.ts
@@ -1,28 +1,19 @@
 import { Request, UnaryResponse } from 'grpc-web';
 import { Message } from 'google-protobuf';
-import { LoginResponse } from "@/protobuf/auth_pb";
+import store from "@/store";
 
 class AuthInterceptor {
-  private token: string;
-
-  constructor(token?: string) {
-    this.token = token ?? "";
-  }
-
   intercept(
     request: Request<Message, Message>,
     invoker: (request: Request<Message, Message>) => Promise<UnaryResponse<Message, Message>>
   ): Promise<UnaryResponse<Message, Message>> {
-    const metadata = request.getMetadata();
-    metadata.Authorization = this.token;
+    const token = store.state.auth.accessToken;
+    if (token) {
+      const metadata = request.getMetadata();
+      metadata.Authorization = token;
+    }
 
-    return invoker(request).then((response: UnaryResponse<Message, Message>) => {
-      const responseMessage = response.getResponseMessage();
-      if (responseMessage instanceof LoginResponse) {
-        this.token = responseMessage.getAccessToken();
-      }
-      return response;
-    });
+    return invoker(request);
   }
 }
 
