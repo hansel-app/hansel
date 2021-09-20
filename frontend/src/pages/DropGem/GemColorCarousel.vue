9c89534d5bd9340291e0d80df6baa7ef82cf6ad5
jialin7878 <E0425498@u.nus.edu>
2021-09-20 21:53:14 +0800

    Update GemColorCarousel to iterate through GemColor

--- frontend/src/pages/DropGem/GemColorCarousel.vue
+++ frontend/src/pages/DropGem/GemColorCarousel.vue
@@ -2,9 +2,9 @@
   <!-- TODO: make this into actual carousel -->
   <Row>
     <Button
-      v-for="gem in mockGems"
-      :key="gem.color"
-      :color="gem.color.selectionColor"
+      v-for="gem in GemColor"
+      :key="gem"
+      :color="getGemColorHex(gem)"
     />
   </Row>
 </template>
@@ -12,13 +12,33 @@
 <script>
 import { defineComponent } from "vue";
 import { Button, Row } from "vant";
-import { mockGems } from "@/interfaces/mockData";
+import { GemColor } from "@/interfaces";
 
 export default defineComponent({
   data() {
     return {
-      mockGems,
+      GemColor,
     };
+  },
+  methods: {
+    getGemColorHex(color) {
+      switch (color) {
+        case GemColor.BLACK:
+          return "#8d8d8d";
+        case GemColor.PURPLE:
+          return "#cdb4db";
+        case GemColor.PINK:
+          return "#ffc8dd";
+        case GemColor.BLUE:
+          return "#a2d2ff";
+        case GemColor.YELLOW:
+          return "#fffb8a";
+        case GemColor.GREEN:
+          return "#a6ffb6";
+        default:
+          return "#FFFFFF";
+      }
+    },
   },
   components: {
     Button,
