83061c18011b6d2f3f8fa93d96268a0847f62446
jialin7878 <E0425498@u.nus.edu>
2021-09-21 12:57:20 +0800

    Add getFriendRequests to user store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,16 +1,19 @@
 import { createStore, Store } from "vuex";
 import authModule, { AuthState } from "./modules/auth";
 import gemsModule, { GemsState } from "./modules/gems";
+import userModule, { UserState } from "./modules/user";
 
 export interface RootState {
   auth: AuthState;
   gems: GemsState;
+  user: UserState;
 }
 
 const store: Store<RootState> = createStore({
   modules: {
     auth: authModule,
     gems: gemsModule,
+    user: userModule,
   },
 });
 
--- frontend/src/store/modules/user.ts
+++ frontend/src/store/modules/user.ts
@@ -1,8 +1,11 @@
 import services from "@/api/services";
+import { User } from "@/interfaces";
 import {
   FriendInfo,
   GetFriendsRequest,
   GetFriendsResponse,
+  GetPendingFriendRequestsRequest,
+  GetPendingFriendRequestsResponse,
 } from "@/protobuf/friend_pb";
 import { RootState } from "@/store";
 import { Module } from "vuex";
@@ -11,24 +14,40 @@ export interface UserState {
   // User's friends.
   // FriendInfo includes their id, username, displayName and avatar (TODO).
   friends: FriendInfo[];
+  friendRequests: FriendInfo[];
+  self: User;
 
-  // TODO: Signed in user
   // TODO: user's current geolocation
 }
 
 const userModule: Module<UserState, RootState> = {
   state: {
     friends: [],
+    friendRequests: [],
+    // TODO: replace hardcode
+    self: {
+      id: 100,
+      username: "maggieMee",
+      displayName: "Maggie Mee",
+      avatar: "avatarrrr",
+    },
   },
   mutations: {
     setFriends(state, friends: FriendInfo[]) {
       state.friends.splice(0, state.friends.length);
       friends.forEach((friend) => state.friends.push(friend));
     },
+    setFriendRequests(state, pendingFriends: FriendInfo[]) {
+      state.friends.splice(0, state.friends.length);
+      pendingFriends.forEach((friendRequest) =>
+        state.friendRequests.push(friendRequest)
+      );
+    },
   },
   actions: {
     getFriends({ commit }) {
       const request = new GetFriendsRequest();
+      request.setUserId(this.state.user.self.id);
 
       return new Promise((resolve, reject) => {
         services.friendsClient
@@ -36,6 +55,21 @@ const userModule: Module<UserState, RootState> = {
           .then((resp: GetFriendsResponse) => {
             const friends: FriendInfo[] = resp.getFriendsList();
             commit("setFriends", friends);
+            resolve(resp);
+          })
+          .catch((err) => reject(err));
+      });
+    },
+    getFriendRequests({ commit }) {
+      const request = new GetPendingFriendRequestsRequest();
+      request.setUserId(this.state.user.self.id);
+
+      return new Promise((resolve, reject) => {
+        services.friendsClient
+          .getFriendRequests(request)
+          .then((resp: GetPendingFriendRequestsResponse) => {
+            const pendingFriends: FriendInfo[] = resp.getFriendsList();
+            commit("setFriendRequests", pendingFriends);
             resolve(resp);
           })
           .catch((err) => reject(err));
