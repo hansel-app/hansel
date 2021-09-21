567cf9e629d3744dea34c4899062f5aedb076e63
jialin7878 <E0425498@u.nus.edu>
2021-09-21 15:37:25 +0800

    Define new FriendRelationship struct
    Open to name suggestions~

--- backend/internal/core/domain/users/user.go
+++ backend/internal/core/domain/users/user.go
@@ -1,9 +1,17 @@
 package users
 
+import "time"
+
 type User struct {
 	ID             int64
 	Username       string
 	DisplayName    string
 	Email          string
 	HashedPassword string
+}
+
+type FriendRelationship struct {
+	Requester   User
+	Receiver    User
+	RequestedAt time.Time
 }
