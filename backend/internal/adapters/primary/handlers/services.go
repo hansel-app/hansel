dc9509898e1e5f448e4195b3da81233879ae2be0
jialin7878 <E0425498@u.nus.edu>
2021-09-21 14:38:23 +0800

    Register FriendServiceServer

--- backend/internal/adapters/primary/handlers/services.go
+++ backend/internal/adapters/primary/handlers/services.go
@@ -8,11 +8,14 @@ import (
 	"github.com/hansel-app/hansel/internal/adapters/secondary/repositories"
 	"github.com/hansel-app/hansel/internal/auth"
 	"github.com/hansel-app/hansel/protobuf/authapi"
+	"github.com/hansel-app/hansel/protobuf/friendsapi"
 	"github.com/hansel-app/hansel/protobuf/gemsapi"
 )
 
 // RegisterServices registers all gRPC services available with the gRPC server.
 func RegisterServices(s *grpc.Server, db *sqlx.DB, jwtManager *auth.JWTManager) {
-	authapi.RegisterAuthServiceServer(s, internal.NewAuthService(repositories.NewUserRepository(db), jwtManager))
+	userRepository := repositories.NewUserRepository(db)
+	authapi.RegisterAuthServiceServer(s, internal.NewAuthService(userRepository, jwtManager))
 	gemsapi.RegisterGemServiceServer(s, internal.NewGemService(repositories.NewGemRepository(db)))
+	friendsapi.RegisterFriendServiceServer(s, internal.NewUserService(userRepository))
 }
