69b6ae052ee552303ed3390b54e464cbcd248f43
jialin7878 <E0425498@u.nus.edu>
2021-09-22 17:07:22 +0800

    Use Header in ProfilePage

--- frontend/src/pages/Profile/ProfilePage.vue
+++ frontend/src/pages/Profile/ProfilePage.vue
@@ -1,23 +1,29 @@
 <template>
-  <NavBar left-arrow @click-left="goBack" />
-  <AvatarHeader
-    :avatarUrl="placeholderAvatarUrl"
-    :displayName="mockSelfUser.displayName"
-    :username="mockSelfUser.username"
-  />
-  <CellGroup>
-    <Cell title="Edit profile" is-link @click="goToEditProfile" />
-    <Cell title="Friends" is-link />
-  </CellGroup>
-  <Cell title="Logout" />
+  <div class="overlay bring-to-front">
+    <Header />
+    <AvatarHeader
+      :avatarUrl="placeholderAvatarUrl"
+      :displayName="mockSelfUser.displayName"
+      :username="mockSelfUser.username"
+    />
+
+    <CellGroup>
+      <Cell title="Edit profile" is-link @click="goToEditProfile" />
+      <Cell title="Friends" is-link />
+    </CellGroup>
+    <Cell title="Logout" />
+  </div>
+  <ProfilePageBg />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { CellGroup, Cell, NavBar } from "vant";
+import { CellGroup, Cell } from "vant";
 import { mockSelfUser, mockFriends } from "@/interfaces/mockData";
 import { EDIT_PROFILE_ROUTE } from "@/constants";
 import AvatarHeader from "./AvatarHeader.vue";
+import ProfilePageBg from "./ProfilePageBg.vue";
+import Header from "@/components/Header.vue";
 
 export default defineComponent({
   data() {
@@ -27,6 +33,7 @@ export default defineComponent({
         "https://media.istockphoto.com/vectors/happy-young-woman-watching-into-rounded-frame-isolated-on-white-3d-vector-id1296058958?b=1&k=20&m=1296058958&s=170667a&w=0&h=6m2FU2hKv6emHjNtdNSqBJR1uMq64smptqwDAZNo6bg=",
       mockSelfUser,
       mockFriends,
+      color: "#ffffff",
     };
   },
   methods: {
@@ -41,7 +48,8 @@ export default defineComponent({
     AvatarHeader,
     CellGroup,
     Cell,
-    NavBar,
+    Header,
+    ProfilePageBg,
   },
 });
 </script>
@@ -49,5 +57,9 @@ export default defineComponent({
 <style scoped lang="less">
 .sub-header {
   text-align: left;
+}
+
+.bring-to-front {
+  z-index: 2;
 }
 </style>
