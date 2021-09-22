bc6a1df39febdd37ac4e481f7635dd9fee7cac87
jialin7878 <E0425498@u.nus.edu>
2021-09-23 00:00:50 +0800

    Fix merge bug

--- frontend/src/pages/GemLogs/LogsList.vue
+++ frontend/src/pages/GemLogs/LogsList.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="background-gradient">
-        <BackSwipe />
-        <Header title="Gem logs"/>
-        <div class="container">
-            <Searchbar placeholder="Search a friend" />
-            <CellGroup>
+  <div class="background-gradient">
+    <BackSwipe />
+    <Header title="Gem logs" />
+    <div class="container">
+      <Searchbar placeholder="Search a friend" />
+      <CellGroup>
         <LogsPreview
           v-for="friend in mockFriends"
           :key="friend.id"
@@ -13,7 +13,6 @@
           :isClickable="true"
         />
       </CellGroup>
-        </div>
     </div>
 </template>
 <script lang="ts">
@@ -21,7 +20,8 @@ import { defineComponent } from "vue";
 import { mockFriends, mockGems } from "@/interfaces/mockData.ts";
 import Header from "@/components/Header.vue";
 import Searchbar from "@/components/Searchbar.vue";
-import BackSwipe from "@/components/BackSwipe.vue"
+import BackSwipe from "@/components/BackSwipe.vue";
+
 import LogsPreview from "./LogsPreview.vue";
 
 export default defineComponent({
