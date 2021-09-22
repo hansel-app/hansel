053c44f5107aaf0ea9fd4df0e1ab84c035572315
ming-00 <e0470728@u.nus.edu>
2021-09-22 10:50:09 +0800

    Create back swipe component

---
+++ frontend/src/components/BackSwipe.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="backswipe-area" @click="goBack" v-touch:swipe.right="goBack">
+    </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+import { HOME_ROUTE } from "@/constants";
+
+export default defineComponent({
+    methods: {
+        goBack() {
+            console.log('backing');
+            return this.$router.push(this.path);
+        }
+    },
+    props: {
+        path: {
+            type: String,
+            default: () => HOME_ROUTE,
+        }
+    }
+})
+</script>
+
+<style scoped>
+.backswipe-area {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    height: 100vh;
+    width: 6%;
+    z-index: 1;
+}
+</style>
