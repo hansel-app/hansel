30408c7886e6ff3124d89d855483b2ea85855f1e
jialin7878 <E0425498@u.nus.edu>
2021-09-22 15:54:29 +0800

    Add input fields for username and display name

--- frontend/src/pages/Profile/EditProfilePage.vue
+++ frontend/src/pages/Profile/EditProfilePage.vue
@@ -5,6 +5,20 @@
   <van-button class="edit-button"
     ><van-icon name="edit" size="40px"
   /></van-button>
+  <van-form>
+    <van-field
+      v-model="username"
+      name="Username"
+      label="Username"
+      placeholder="username"
+    />
+    <van-field
+      v-model="displayName"
+      name="Display name"
+      label="Display name"
+      placeholder="Display name"
+    />
+  </van-form>
 </template>
 
 <script lang="ts">
@@ -21,6 +35,8 @@ export default defineComponent({
         "https://media.istockphoto.com/vectors/happy-young-woman-watching-into-rounded-frame-isolated-on-white-3d-vector-id1296058958?b=1&k=20&m=1296058958&s=170667a&w=0&h=6m2FU2hKv6emHjNtdNSqBJR1uMq64smptqwDAZNo6bg=",
       mockSelfUser,
       mockFriends,
+      username: mockSelfUser.username,
+      displayName: mockSelfUser.displayName,
     };
   },
   methods: {
