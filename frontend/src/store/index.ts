3596c2704c62503a0a12da622abe7923bf5a9ae1
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-22 22:48:29 +0800

    Add persistent state

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,9 @@
+import * as Cookies from "js-cookie";
 import { createStore, Store } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 import authModule, { AuthState } from "./modules/auth";
 import gemsModule, { GemsState } from "./modules/gems";
 import userModule, { UserState } from "./modules/user";
-
 export interface RootState {
   auth: AuthState;
   gems: GemsState;
@@ -15,6 +16,14 @@ const store: Store<RootState> = createStore({
     gems: gemsModule,
     user: userModule,
   },
+
+  plugins: [
+    createPersistedState({
+      getState: (key) => Cookies.getJSON(key),
+      setState: (key, state) =>
+        Cookies.set(key, state, { expires: 3, secure: true }),
+    }),
+  ],
 });
 
 export default store;
