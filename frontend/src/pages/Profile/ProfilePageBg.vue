e41df194510621d07178de7365c3be5d54c51bee
jialin7878 <E0425498@u.nus.edu>
2021-09-22 17:06:06 +0800

    Create ProfilePageBg
    it's not perfect but if i spend any more time on this I'm going to ascend

---
+++ frontend/src/pages/Profile/ProfilePageBg.vue
@@ -0,0 +1,28 @@
+<template>
+  <SemiCircleBg class="semi-circle overlay" />
+  <div class="blue-gradient-bg" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SemiCircleBg from "../../components/SemiCircleBg.vue";
+
+export default defineComponent({
+  components: {
+    SemiCircleBg,
+  },
+});
+</script>
+
+<style SemiCircleBg scoped lang="less">
+.blue-gradient-bg {
+  background: linear-gradient(to bottom, #c0c2e5, #e43d9700);
+  height: 100vh;
+  position: relative;
+}
+
+.semi-circle {
+  background-color: #ffffff;
+  position: absolute;
+}
+</style>
