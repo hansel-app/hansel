ed6e5386d4837c1aca49edf992afe08bf0a11a9f
jialin7878 <E0425498@u.nus.edu>
2021-09-22 20:54:47 +0800

    Create LogsPreview component

---
+++ frontend/src/pages/GemLogs/LogsPreview.vue
@@ -0,0 +1,25 @@
+<template>
+  <FriendCell :friend="friend" :isClickable="true" labelOverride="test" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { User, Gem } from "@/interfaces";
+import FriendCell from "@/components/FriendCell.vue";
+
+export default defineComponent({
+  props: {
+    friend: {
+      type: User,
+      required: true,
+    },
+    mostRecentGemActivity: {
+      type: Gem,
+      required: true,
+    },
+  },
+  components: {
+    FriendCell,
+  },
+});
+</script>
