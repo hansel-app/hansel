79346dc0c8c3a644cfba2ee354b2b626f2981ae7
Ian Yong <ianyongyc@gmail.com>
2021-09-22 17:34:56 +0800

    Add display_name & avatar columns to database

--- backend/internal/core/domain/users/user.go
+++ backend/internal/core/domain/users/user.go
@@ -5,10 +5,10 @@ import "time"
 type User struct {
 	ID             int64  `db:"id"`
 	Username       string `db:"username"`
-	DisplayName    string
+	DisplayName    string `db:"display_name"`
 	Email          string `db:"email"`
 	HashedPassword string `db:"hashed_password"`
-	Avatar         []byte
+	Avatar         []byte `db:"avatar"`
 }
 
 type FriendRelationship struct {
