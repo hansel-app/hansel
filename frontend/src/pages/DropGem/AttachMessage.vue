66088e3968b1bb86bf7307e86b6ad70353c8cc7d
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-22 08:01:04 +0800

    Pass color props into header

--- frontend/src/pages/DropGem/AttachMessage.vue
+++ frontend/src/pages/DropGem/AttachMessage.vue
@@ -1,27 +1,37 @@
 <template>
-  <GemHeader title="Drop a gem" />
-  <div class="container">
-    <p class="sub-header">Attach a message!</p>
-    <Field
-      class="message-input"
-      placeholder="Enter your message"
-      type="textarea"
-      rows="5"
-      @input="this.$emit('set-message-event', $event.target.value)"
-    />
+  <div>
+    <GemHeader title="Drop a gem" :color="color" />
+    <div class="container">
+      <p class="sub-header">Attach a message!</p>
+      <Field
+        class="message-input"
+        placeholder="Enter your message"
+        type="textarea"
+        rows="5"
+        @input="this.$emit('set-message-event', $event.target.value)"
+      />
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import { Field } from "vant";
 import GemHeader from "@/components/GemHeader.vue";
 import { HOME_ROUTE } from "@/constants";
+import { GemColor } from "@/interfaces";
+import { mapState } from "vuex";
 
 export default defineComponent({
   components: {
     Field,
     GemHeader,
+  },
+
+  computed: {
+    ...mapState({
+      color: (state: any) => state.gems.dropGemFormState.color as GemColor,
+    }),
   },
   methods: {
     dropGem: function() {
