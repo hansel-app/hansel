a414c6222f6d1fe55161273fddd33ad2e26bda17
jialin7878 <E0425498@u.nus.edu>
2021-09-23 04:44:57 +0800

    Use slot for friend info in LogsDetailsPage

--- frontend/src/pages/GemLogs/LogsDetails.vue
+++ frontend/src/pages/GemLogs/LogsDetails.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <Header title="ah" :backLink="GEM_LOGS_ROUTE" />
-    <FriendCell :friend="friend" />
+    <Header :backLink="GEM_LOGS_ROUTE">
+      <p>{{ friend.displayName }}</p>
+      <CircleAvatar :avatarUrl="placeholderAvatarUrl" />
+    </Header>
+
     <GemMessage v-for="gem in gems" :key="gem.id" :message="gem.getMessage()" />
   </div>
 </template>
@@ -12,19 +15,21 @@ import { GEM_LOGS_ROUTE } from "@/constants";
 import { Gem, GemLogsWithFriend } from "@/protobuf/gem_pb";
 import { User } from "@/interfaces/user";
 import { mockGemLogsWithFriend } from "@/interfaces/mockDataPb";
+import CircleAvatar from "@/components/CircleAvatar.vue";
 import Header from "@/components/Header.vue";
 import GemMessage from "./GemMessage.vue";
-import FriendCell from "@/components/FriendCell.vue";
 
 export default defineComponent({
   components: {
     Header,
+    CircleAvatar,
     GemMessage,
-    FriendCell,
   },
   data() {
     return {
       GEM_LOGS_ROUTE,
+      placeholderAvatarUrl:
+        "https://cdn.mos.cms.futurecdn.net/JycrJzD5tvbGHWgjtPrRZY-970-80.jpg.webp",
     };
   },
   computed: {
@@ -38,7 +43,7 @@ export default defineComponent({
     },
     friend(): User {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return (this.gemLogs.getFriend()!).toObject();
+      return this.gemLogs.getFriend()!.toObject();
     },
   },
 });
