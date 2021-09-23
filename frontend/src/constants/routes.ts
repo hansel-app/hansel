0579f568b6a607a4a50e85e49bda3c17985bb0d1
jialin7878 <E0425498@u.nus.edu>
2021-09-23 17:53:08 +0800

    Add FriendListPage to router

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -5,6 +5,7 @@ export const PROFILE_ROUTE = "/profile";
 export const EDIT_PROFILE_ROUTE = "/profile/edit";
 export const ADD_FRIENDS_ROUTE = "/add-friends";
 export const FRIEND_REQUESTS_ROUTE = "/friend-requests";
+export const FRIEND_LIST_ROUTE = "/friends-list";
 export const LOGIN_ROUTE = "/login";
 export const REGISTER_ROUTE = "/register";
 export const GEM_LOGS_ROUTE = "/logs";
