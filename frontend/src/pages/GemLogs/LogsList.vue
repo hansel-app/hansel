9ad2fa71710a49f3e83780a87090f54bdd0ec1a6
Sebastian Toh <sebas.tsj.98@gmail.com>
2021-09-23 16:47:43 +0800

    Add searching in log lists page

--- frontend/src/pages/GemLogs/LogsList.vue
+++ frontend/src/pages/GemLogs/LogsList.vue
@@ -3,11 +3,11 @@
     <BackSwipe />
     <Header title="Gem logs" />
     <div class="container">
-      <Searchbar placeholder="Search a friend" />
+      <Searchbar placeholder="Search a friend" @input="onSearchQueryInput" />
       <CellGroup>
         <LogsPreview
-          v-for="friend in mockFriends"
-          :key="friend.id"
+          v-for="friend in state.filteredFriends"
+          :key="friend.userId"
           :friend="friend"
           :mostRecentGemActivity="mockSingleGem"
           :isClickable="true"
@@ -17,14 +17,15 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
-import { mockFriends, mockGems } from "@/interfaces/mockData.ts";
+import { defineComponent, reactive } from "vue";
+import { mockFriends, mockGems } from "@/interfaces/mockData";
 import Header from "@/components/Header.vue";
 import Searchbar from "@/components/Searchbar.vue";
 import BackSwipe from "@/components/BackSwipe.vue";
 import { CellGroup } from "vant";
 
 import LogsPreview from "./LogsPreview.vue";
+import { User } from "@/interfaces";
 
 export default defineComponent({
   components: {
@@ -34,12 +35,44 @@ export default defineComponent({
     BackSwipe,
     CellGroup,
   },
-  data() {
-    return {
+  setup() {
+    const allFriends = mockFriends;
+
+    const state = reactive({
+      searchQuery: "",
       isCloseWindow: true,
-      mockFriends,
+      // TODO: replace mockfriends and mockGems
+      filteredFriends: allFriends as User[],
+    });
+
+    return {
+      state,
+      allFriends,
       mockSingleGem: mockGems[0],
     };
+  },
+  methods: {
+    onSearchQueryInput(event: Event) {
+      const target = event.target as HTMLInputElement;
+      this.state.searchQuery = target.value;
+      this.filterFriends();
+    },
+    filterFriends() {
+      if (this.state.searchQuery.length === 0) {
+        this.state.filteredFriends = this.allFriends;
+        return;
+      }
+
+      this.state.filteredFriends = this.allFriends.filter((friend: User) => {
+        const matchUsername = friend.username
+          .toLowerCase()
+          .includes(this.state.searchQuery.toLowerCase());
+        const matchDisplayname = friend.displayName
+          .toLowerCase()
+          .includes(this.state.searchQuery.toLowerCase());
+        return matchUsername || matchDisplayname;
+      });
+    },
   },
 });
 </script>
