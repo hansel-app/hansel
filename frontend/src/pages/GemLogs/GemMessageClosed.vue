02963a00ebaa485b696406264355824721340f3b
jialin7878 <E0425498@u.nus.edu>
2021-09-24 03:03:43 +0800

    Change gem icon based on color

--- frontend/src/pages/GemLogs/GemMessageClosed.vue
+++ frontend/src/pages/GemLogs/GemMessageClosed.vue
@@ -2,18 +2,10 @@
   <van-row :justify="justify" class="fill-width">
     <p v-if="isSentBySelf" class="gem-message" v-bind:style="gemMessageStyle">
       New gem!
-      <van-icon
-        class="gem-icon"
-        :name="require('@/assets/images/purple_64.png')"
-        @click="goToDropGem"
-      />
+      <van-icon class="gem-icon" :name="getGemImageUrl" @click="goToDropGem" />
     </p>
     <p v-else class="gem-message" v-bind:style="gemMessageStyle">
-      <van-icon
-        class="gem-icon"
-        :name="require('@/assets/images/purple_64.png')"
-        @click="goToDropGem"
-      />
+      <van-icon class="gem-icon" :name="getGemImageUrl" @click="goToDropGem" />
       New gem!
     </p>
   </van-row>
@@ -21,7 +13,8 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
-import { Gem } from "@/interfaces";
+import { Gem, GemColor } from "@/interfaces";
+import { getEnumKeyByEnumValue } from "@/utils/enum";
 
 export default defineComponent({
   props: {
@@ -40,6 +33,12 @@ export default defineComponent({
     isSentBySelf: {
       type: Boolean,
       required: true,
+    },
+  },
+  computed: {
+    getGemImageUrl(): string {
+      const gemColorName = getEnumKeyByEnumValue(GemColor, this.gem.color);
+      return require(`@/assets/images/${gemColorName.toLowerCase()}_2048.png`);
     },
   },
 });
