6c2b62ed8faa5a45b793f2f2a02c7fb9f765f5dd
jialin7878 <E0425498@u.nus.edu>
2021-09-24 12:10:48 +0800

    Create WelcomeGem

---
+++ frontend/src/pages/LoginRegister/WelcomeGem.vue
@@ -0,0 +1,24 @@
+<template>
+  <van-image :src="getGemImageUrl" />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { getEnumKeyByEnumValue } from "@/utils/enum";
+import { GemColor } from "@/interfaces";
+
+export default defineComponent({
+  props: {
+    gemColor: {
+      type: Object as PropType<GemColor>,
+      required: true,
+    },
+  },
+  computed: {
+    getGemImageUrl(): string {
+      const gemColorName = getEnumKeyByEnumValue(GemColor, this.gemColor);
+      return require(`@/assets/images/${gemColorName.toLowerCase()}_64.png`);
+    },
+  },
+});
+</script>
