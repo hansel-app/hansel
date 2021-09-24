f0823ca743a10d0accd41014b917abdca1a45054
jialin7878 <E0425498@u.nus.edu>
2021-09-24 11:55:48 +0800

    Add background and panel to LoginPage

--- frontend/src/pages/LoginRegister/LoginPage.vue
+++ frontend/src/pages/LoginRegister/LoginPage.vue
@@ -1,29 +1,39 @@
 <template>
-  <h1>Login</h1>
-  <div class="container">
-    <van-form @submit="handleLogin">
-      <van-field
-        v-model="username"
-        name="Username"
-        placeholder="Username"
-        :rules="[{ required: true, message: 'Username is required' }]"
-      />
-      <van-field
-        v-model="password"
-        type="password"
-        name="Password"
-        placeholder="Password"
-        :rules="[{ required: true, message: 'Password is required' }]"
-      />
-      <div class="button-container content">
-        <van-button round block type="primary" native-type="submit" size="large">
-          Login
-        </van-button>
+  <div class="background-gradient base">
+    <div class="drawer">
+      <h1>Login</h1>
+      <div class="container">
+        <van-form @submit="handleLogin">
+          <van-field
+            v-model="username"
+            name="Username"
+            placeholder="Username"
+            :rules="[{ required: true, message: 'Username is required' }]"
+          />
+          <van-field
+            v-model="password"
+            type="password"
+            name="Password"
+            placeholder="Password"
+            :rules="[{ required: true, message: 'Password is required' }]"
+          />
+          <div class="button-container content">
+            <van-button
+              round
+              block
+              type="primary"
+              native-type="submit"
+              size="large"
+            >
+              Login
+            </van-button>
+          </div>
+          <span class="register" @click="goToRegisterPage">
+            Sign up
+          </span>
+        </van-form>
       </div>
-      <span class="register" @click="goToRegisterPage">
-        Sign up
-      </span>
-    </van-form>
+    </div>
   </div>
 </template>
 
@@ -67,5 +77,28 @@ export default defineComponent({
 }
 .register {
   padding: 0.5em 1em;
+}
+
+.drawer {
+  color: black;
+  background-color: rgba(255, 255, 255, 0.473);
+  backdrop-filter: blur(5px);
+  border-radius: 2em 2em 2em 2em;
+
+  float: left;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  right: 0;
+  padding: 0.5em;
+
+  transition: max-height 0.3s;
+
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+
+  margin-right: 5vw;
+  margin-left: 5vw;
 }
 </style>
